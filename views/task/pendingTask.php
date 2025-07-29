7070d36d272579a62710cc47ca696676679dd1ea
Miguel <magonzalez6786@misena.edu.co>
2025-07-28 22:52:45 -0500

    lista paciente

--- views/task/pendingTask.php
+++ views/task/pendingTask.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/pendingTask.css">
+    <link rel="stylesheet" href="../../css/pendingTask.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.min.css" integrity="sha512-XcIsjKMcuVe0Ucj/xgIXQnytNwBttJbNjltBV18IOnru2lDPe9KRRyvCXw6Y5H415vbBLRm8+q6fmLUU7DfO6Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>Pendientes</title>
